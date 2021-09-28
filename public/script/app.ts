feb7f10be2deedc141f5906120e692cb76cbbcca
unknown <andrej.germic@gmail.com>
2021-09-28 18:33:45 +0200

    Created card animation in js

--- public/script/app.ts
+++ public/script/app.ts
@@ -2,29 +2,22 @@
 // const staticTop = document.getElementById("staticTop");
 // const staticBottom = document.getElementById("staticBottom");
 const movingCard = document.getElementById("movingCard");
+const timeout = 600;
 
 const rotatingCard = () => {
   // Animations enabled
   // Go to start position
   if (movingCard) {
+    movingCard.style.transform = "perspective(300px) rotateX(-90deg)";
     setTimeout(() => {
-      movingCard.style.transform = "perspective(300px) rotateX(-10deg)";
+      movingCard.style.transition = "0s all linear";
+      movingCard.style.transform = "perspective(300px) rotateX(90deg)";
       setTimeout(() => {
-        movingCard.style.transform = "perspective(300px) rotateX(-90deg)";
-        setTimeout(() => {
-          movingCard.style.transition = "0s all linear";
-          movingCard.style.transform = "perspective(300px) rotateX(90deg)";
-          setTimeout(() => {
-            movingCard.style.transition = "0.3s all linear";
-            movingCard.style.transform = "perspective(300px) rotateX(10deg)";
-            setTimeout(() => {
-              movingCard.style.transform = "perspective(300px) rotateX(0deg)";
-            }, 500);
-          }, 10);
-        }, 500);
-      }, 50);
-    }, 50);
+        movingCard.style.transition = "0.3s all linear";
+        movingCard.style.transform = "perspective(300px) rotateX(0deg)";
+      }, 30);
+    }, timeout / 2);
   }
 };
 
-setInterval(() => rotatingCard(), 6000);
+setInterval(() => rotatingCard(), 1000);
